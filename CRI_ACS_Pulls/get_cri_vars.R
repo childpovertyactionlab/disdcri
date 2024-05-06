726cf888055729bf206aee8a53223c3eab15b360
Joseph Mohr <jmohr@thenightwatch.co>
2024-05-06 00:05:26 -0500

    Create new centrally located var list

---
+++ CRI_ACS_Pulls/get_cri_vars.R
@@ -0,0 +1,50 @@
+# CRI ACS Community Variables ----
+comm_variables <- c(
+  comm_thh = "B25106_001",
+  comm_oohh = "B25106_002",
+  comm_rohh = "B25106_024",
+  comm_ocb1 = "B25106_006",
+  comm_ocb2 = "B25106_010",
+  comm_ocb3 = "B25106_014",
+  comm_ocb4 = "B25106_018",
+  comm_ocb5 = "B25106_022",
+  comm_rcb1 = "B25106_028",
+  comm_rcb2 = "B25106_032",
+  comm_rcb3 = "B25106_036",
+  comm_rcb4 = "B25106_040",
+  comm_rcb5 = "B25106_044",
+  comm_bb1 = "B28005_005",
+  comm_bb2 = "B28005_011",
+  comm_bb3 = "B28005_017",
+  comm_bb4 = "B28005_001",
+  comm_bb5 = "B28005_002"
+)
+
+# CRI ACS Economics Variables no transformation ----
+econ_Svariables <- c(
+  econ_adupop = "B23001_001",
+  econ_juvpop1 = "B23001_003",
+  econ_juvpop2 = "DP05_0009",
+  econ_pop = "S1701_C01_001",
+  econ_belpov = "S1701_C02_001",
+  econ_kids = "S1701_C01_002",
+  econ_cbp = "S1701_C02_002",
+  econ_u5 = "S1701_C01_003",
+  econ_u5bp = "S1701_C02_003",
+  econ_5t17 = "S1701_C01_004",
+  econ_5t17bp = "S1701_C02_004",
+  econ_popov16 = "S0101_C01_025"
+)
+
+econ_medinc <- c(
+  econ_medinc = "B19013_001",
+  econ_unem = "B23025_005",
+  econ_juvunemM1 = "B23001_008",
+  econ_juvunemM2 = "B23001_015",
+  econ_juvunemM3 = "B23001_022",
+  econ_juvunemF1 = "B23001_094",
+  econ_juvunemF2 = "B23001_101",
+  econ_juvunemF3 = "B23001_108"
+)
+
+

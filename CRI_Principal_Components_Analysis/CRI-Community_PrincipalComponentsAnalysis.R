40354084f1b533bb34d0f959056ec4753a3f9e32
Joseph Mohr <jmohr@thenightwatch.co>
2024-07-21 22:24:38 -0500

    Refactor

--- CRI_Principal_Components_Analysis/CRI-Community_PrincipalComponentsAnalysis.R
+++ CRI_Principal_Components_Analysis/CRI-Community_PrincipalComponentsAnalysis.R
@@ -1,57 +1,54 @@
-rm(list=ls(all=TRUE))
-setwd("SET AS WORKING DIRECTORY OF DOWNLOADED FILE LOCATIONS")
-library(tidyverse)
-library(rio)
-library(factoextra)
+source("toolkit.R")
 
-#import indicator files
+# import indicator files
 community <- import("CRI Community Buffer Variables Normalized.csv")
 tea_sln <- select(community, "TEA", "SLN")
 
-#perform PCA on health to evaluate correct dimensions
-comm.pca <- prcomp(community[,c(3:18)], center = TRUE, scale = FALSE)  #scale false as normalization has already occured
-summary(comm.pca)                                                      #proportion of variance is the important indicator, it reveals which PC are the most valued.
-fviz_eig(comm.pca)                                                     #scree plot visualization depicting what percentage of variance is explained by each principal component.
-fviz_pca_var(comm.pca,                                                 #plots all variables against first and scommd dimension to visualize contribution of each variable to PCA.
-             col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE)     # Avoid text overlapping
+# perform PCA on health to evaluate correct dimensions
+comm.pca <- prcomp(community[, c(3:18)], center = TRUE, scale = FALSE) # scale false as normalization has already occured
+summary(comm.pca) # proportion of variance is the important indicator, it reveals which PC are the most valued.
+fviz_eig(comm.pca) # scree plot visualization depicting what percentage of variance is explained by each principal component.
+fviz_pca_var(comm.pca, # plots all variables against first and scommd dimension to visualize contribution of each variable to PCA.
+  col.var = "contrib", # Color by contributions to the PC
+  gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+  repel = TRUE
+) # Avoid text overlapping
 
 comm.eig <- get_eigenvalue(comm.pca)
-comm.eig
 
-#PCA by variable, extract quality of representation (cos2) of each variable
-#cos2 will reveal the importance of each variable by dimension for the creation of the sub-index further ahead.
+
+# PCA by variable, extract quality of representation (cos2) of each variable
+# cos2 will reveal the importance of each variable by dimension for the creation of the sub-index further ahead.
 commvar <- get_pca_var(comm.pca)
 commvar.name <- rownames(commvar$cos2)
-commV.cos <- as_tibble(cbind(commvar.name, commvar$cos2))   
-rm(commvar.name)
+commV.cos <- as_tibble(cbind(commvar.name, commvar$cos2))
 
-#PCA by individual, extract quality of representation (cos2) of each campus
-#cos2 will reveal the importance of each school campus by dimension for the creation of the sub-index further ahead.
+# PCA by individual, extract quality of representation (cos2) of each campus
+# cos2 will reveal the importance of each school campus by dimension for the creation of the sub-index further ahead.
 commind <- get_pca_ind(comm.pca)
-commind.cos <- cbind(tea_sln, as_tibble(commind$cos2))     #coordinates
+commind.cos <- cbind(tea_sln, as_tibble(commind$cos2)) # coordinates
 commI.cos <- left_join(community, commind.cos)
 
-#community Sub-Index Creation
+# community Sub-Index Creation
 summary(comm.pca)
-commV.cos
-colnames(commI.cos)
+
 ci_fin <- commI.cos %>%
-  mutate(ci_index = ((ci_bbp+ci_evrate+ci_incarpct+ci_ltrvp+ci_ocbp+ci_parkcap+ci_rvp+ci_u18bbp)*(Dim.1))+((ci_bvp+ci_ltbvp+ci_ocb+ci_rcb+ci_rcbp)*(Dim.2))+((ci_cctrcap+ci_juvcrimecap+ci_libcap)*(Dim.3))) %>%
+  mutate(ci_index = ((ci_bbp + ci_evrate + ci_incarpct + ci_ltrvp + ci_ocbp + ci_parkcap + ci_rvp + ci_u18bbp) * (Dim.1)) + ((ci_bvp + ci_ltbvp + ci_ocb + ci_rcb + ci_rcbp) * (Dim.2)) + ((ci_cctrcap + ci_juvcrimecap + ci_libcap) * (Dim.3))) %>%
   select(everything(.), "TEA", "SLN", names(.), -(Dim.1:Dim.16))
 
-# OWEN I'm unsure about ocb/rcb and ocbp/rcbp being included in the index but the eigenvalue plot earlier in the PCA shows them in pretty disparate areas, your input would be appreciated.
+# OWEN I'm unsure about ocb/rcb and ocbp/rcbp being included in the index
+# but the eigenvalue plot earlier in the PCA shows them in pretty disparate areas,
+# your input would be appreciated.
 
-#                    ##########################################################
-#                    # Dim.1 ~34% # ci_bbp, ci_evrate, ci_incarpct, ci_ltrvp  #
-#                    #            # ci_ocbp, ci_parkcap, ci_rvp, ci_u18bbp    #
-#                    # Dim.2 ~18% # ci_bvp, ci_ltbvp, ci_ocb, ci_rcb, ci_rcbp #
-#                    # Dim.3 ~13% # ci_cctrcap, ci_juvcrimecap, ci_libcap     #
-#                    # Dim.4 ~9%  #                                           #
-#                    ##########################################################
+###########################################################
+## Dim.1 ~34% # ci_bbp, ci_evrate, ci_incarpct, ci_ltrvp  #
+##            # ci_ocbp, ci_parkcap, ci_rvp, ci_u18bbp    #
+## Dim.2 ~18% # ci_bvp, ci_ltbvp, ci_ocb, ci_rcb, ci_rcbp #
+## Dim.3 ~13% # ci_cctrcap, ci_juvcrimecap, ci_libcap     #
+## Dim.4 ~9%  #                                           #
+###########################################################
 
-#export tables
+# export tables
 export(commI.cos, "SET AS EXPORT DIRECTORY/CRI Community - PCA Indiviual Cos.csv")
 export(commV.cos, "SET AS EXPORT DIRECTORY/CRI Community - PCA Variable Cos.csv")
 export(ci_fin, "SET AS EXPORT DIRECTORY/CRI Community - Community Sub-Index.csv")
--- CRI_Principal_Components_Analysis/CRI-Health_PrincipalComponentsAnalysis.R
+++ CRI_Principal_Components_Analysis/CRI-Health_PrincipalComponentsAnalysis.R
@@ -1,63 +1,73 @@
-rm(list=ls(all=TRUE))
-setwd("SET AS WORKING DIRECTORY OF DOWNLOADED FILE LOCATIONS")
-library(tidyverse)
-library(rio)
-library(factoextra)
+source("toolkit.R")
 
-#import indicator files
+# import indicator files
 health <- import("CRI Health Buffer Variables Normalized.csv")
 health.ghi <- import("CRI Health CDC500 - General Health Index.csv")
-tea_sln <- select(health, "TEA", "SLN")
+tea_sln <- health %>% select(TEA, SLN)
 
 colnames(health)
-#Plot of Private vs Public Insurance
-ggplot(health) + 
+# Plot of Private vs Public Insurance
+ggplot(health) +
   geom_point(aes(x = hi_perpri, y = hi_perpub)) +
   geom_smooth(aes(x = hi_perpri, y = hi_perpub))
 
-ggplot(health) + 
+ggplot(health) +
   geom_histogram(aes(hi_prtopu))
 
-#New selection of variables to conduct PCA on
+# New selection of variables to conduct PCA on
 health.in <- left_join(health, health.ghi) %>%
-  select("TEA", "SLN", "hi_clincap", "hi_fruitsveggies", "hi_groccap", "hi_le", "hi_perunin", "hi_pharmacap", "hi_prtopu", "hi_mhlth", "hi_phlth", "hi_gen")
+  select(
+    TEA,
+    SLN,
+    hi_clincap,
+    hi_fruitsveggies,
+    hi_groccap,
+    hi_le,
+    hi_perunin,
+    hi_pharmacap,
+    hi_prtopu,
+    hi_mhlth,
+    hi_phlth,
+    hi_gen
+  )
 
-#perform PCA on health to evaluate correct dimensions
-hel.pca <- prcomp(health.in[,c(3:12)], center = TRUE, scale = FALSE) #scale false as normalization has already occured
-summary(hel.pca)                                                      #proportion of variance is the important indicator, it reveals which PC are the most valued.
-fviz_eig(hel.pca)                                                     #scree plot visualization depicting what percentage of variance is explained by each principal component.
-fviz_pca_var(hel.pca,                                                 #plots all variables against first and second dimension to visualize contribution of each variable to PCA.
-             col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE)     # Avoid text overlapping
+# perform PCA on health to evaluate correct dimensions
+hel.pca <- prcomp(health.in[, c(3:12)], center = TRUE, scale = FALSE) # scale false as normalization has already occured
+summary(hel.pca) # proportion of variance is the important indicator, it reveals which PC are the most valued.
+fviz_eig(hel.pca) # scree plot visualization depicting what percentage of variance is explained by each principal component.
+fviz_pca_var(hel.pca, # plots all variables against first and second dimension to visualize contribution of each variable to PCA.
+  col.var = "contrib", # Color by contributions to the PC
+  gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+  repel = TRUE
+) # Avoid text overlapping
 
 hel.eig <- get_eigenvalue(hel.pca)
 hel.eig
 
-#PCA by variable, extract quality of representation (cos2) of each variable
-#cos2 will reveal the importance of each variable by dimension for the creation of the sub-index further ahead.
+# PCA by variable, extract quality of representation (cos2) of each variable
+# cos2 will reveal the importance of each variable by dimension for the creation of the sub-index further ahead.
 helvar <- get_pca_var(hel.pca)
 helvar.name <- rownames(helvar$cos2)
 helV.cos <- as_tibble(cbind(helvar.name, helvar$cos2))
 rm(helvar.name)
-head(helV.cos)                                                              #quality of representation (cos2)
+head(helV.cos) # quality of representation (cos2)
 
-#PCA by individual, extract quality of representation (cos2) of each campus
-#cos2 will reveal the importance of each school campus by dimension for the creation of the sub-index further ahead.
+# PCA by individual, extract quality of representation (cos2) of each campus
+# cos2 will reveal the importance of each school campus by dimension for the creation of the sub-index further ahead.
 helind <- get_pca_ind(hel.pca)
-helind.cos <- cbind(tea_sln, as_tibble(helind$cos2))   #quality of representation
+helind.cos <- cbind(tea_sln, as_tibble(helind$cos2)) # quality of representation
 helI.cos <- left_join(health.in, helind.cos)
 head(helI.cos)
 
-#Health Sub-Index Creation
+# Health Sub-Index Creation
 summary(hel.pca)
 helV.cos
 colnames(helI.cos)
 hi_fin <- helI.cos %>%
-  mutate(hi_index = ((hi_prtopu+hi_perunin+hi_mhlth+hi_phlth+hi_gen)*(Dim.1))+ ((hi_clincap+hi_groccap+hi_le+hi_pharmacap)*(Dim.2))+(hi_fruitsveggies*Dim.3)) %>%
+  mutate(hi_index = ((hi_prtopu + hi_perunin + hi_mhlth + hi_phlth + hi_gen) * (Dim.1)) + ((hi_clincap + hi_groccap + hi_le + hi_pharmacap) * (Dim.2)) + (hi_fruitsveggies * Dim.3)) %>%
   select(everything(.), "TEA", "SLN", names(.), -(Dim.1:Dim.10))
 
-#Dropped perins variable and kept per unins instead for PCA
+# Dropped perins variable and kept per unins instead for PCA
 
 #                      ###########################################
 #                      # Dim.1 ~62% # hi_prtopu, hi_perunin      #
@@ -68,7 +78,7 @@ hi_fin <- helI.cos %>%
 #                      # Dim.4 ~5%  #                            #
 #                      ###########################################
 
-#export tables
+# export tables
 export(helI.cos, "SET AS EXPORT DIRECTORY/CRI Health - PCA Indiviual Cos.csv")
 export(helV.cos, "SET AS EXPORT DIRECTORY/CRI Health - PCA Variable Cos.csv")
 export(hi_fin, "SET AS EXPORT DIRECTORY/CRI Health - Health Sub-Index.csv")
